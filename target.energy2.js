664108332f4e987679ef15767d44a399a74d601d
Alexandr <alex@jurist.vn.ua>
2018-12-14 10:16:28 +0200

    console

--- target.energy2.js
+++ target.energy2.js
@@ -5,6 +5,7 @@ function TargetEnergy2(creep){
  //if(!creep.memmory.target){
  var sources = creep.room.storage;
  if(!sources){
+   creep.say('0000');
    var sources = creep.room.find(FIND_STRUCTURES, {
                              filter: (structure) => {
                              return (structure.structureType == STRUCTURE_CONTAINER)
@@ -16,11 +17,13 @@ function TargetEnergy2(creep){
    }
  }
  if(!sources){
+   creep.say('1111');
    var sources = creep.room.find(FIND_SOURCES);
      if(creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
        creep.moveTo(sources[0], {visualizePathStyle: {stroke: '#ffaa00'}});
      }
  } else {
+   creep.say('2222');
    if(creep.withdraw(sources[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
      creep.moveTo(sources[0]);
    }
