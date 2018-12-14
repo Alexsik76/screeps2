5bd4de49e0356b05cb4973ccc9f1b57f7522115b
Alexandr <alex@jurist.vn.ua>
2018-12-14 23:47:14 +0200

    0

--- main.js
+++ main.js
@@ -143,7 +143,7 @@ console.log('Ready To Invasion 2 = ' + readyToInvasion);
     if(attackers.length > 3 && !defendRoom(Memory.roomName)){
     Memory.invasion = true;
     }
-    if (attackers.length < 2){
+    if (attackers.length < 0){
     Memory.invasion = false;
     }
 //   Memory.invasion = true;
