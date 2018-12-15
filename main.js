f63b1ac458b89c2966b4ce508b09bd871e9fe9c6
Alexandr <alex@jurist.vn.ua>
2018-12-15 08:22:19 +0200

    f

--- main.js
+++ main.js
@@ -154,7 +154,7 @@ console.log('Ready To Invasion 2 = ' + readyToInvasion);
       }
     }
     if(attackers.length > 7 && rattackers.length > 5 && !defendRoom(Memory.roomName)){
-    Memory.invasion = true;
+    Memory.invasion = false;
     }
     if (attackers.length < 0 && rattackers.length < 0){
     Memory.invasion = false;
