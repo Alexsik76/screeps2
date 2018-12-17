af1966a9f0ddd89bec648e9b0e3c09b9b4f1e153
Alexandr <alex@jurist.vn.ua>
2018-12-17 14:15:15 +0200

    6

--- building.js
+++ building.js
@@ -3,8 +3,7 @@ function letBuild(roomName) {
     //console.log('Continers and Links = ' + !Memory.linksId);
     if(!Memory.linksId || (Memory.containersId.length > 0 &&
       Memory.linksId.length <  Memory.containersId.length)){
-      console.log('Conainers and Links = '
-      + Memory.containersId.length + " " + Memory.linksId.length);
+      
     }
   }
 
