a6b18e6900fd944e8a6dff4bd997c43f1e5ece94
Alexandr <alex@jurist.vn.ua>
2018-12-17 14:09:31 +0200

    5

--- building.js
+++ building.js
@@ -1,10 +1,11 @@
 function letBuild(roomName) {
   if(Game.getObjectById(Memory.controller).level > 4 ){
-    if((Memory.containersId.length > 0 &&
-      Memory.linksId.length <  Memory.containersId.length) || !Memory.linksId){
-      conslole.log('Conainers and Links = '
-      + Memory.containersId.length + " " + Memory.linksId.length);
-    }
+    conslole.log('Conainers and Links = ' + !Memory.linksId);
+    //if((Memory.containersId.length > 0 &&
+    //  Memory.linksId.length <  Memory.containersId.length) || !Memory.linksId){
+    //  conslole.log('Conainers and Links = '
+    //  + Memory.containersId.length + " " + Memory.linksId.length);
+    //}
   }
 
 
