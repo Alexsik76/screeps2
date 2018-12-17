90b65b267f3a811e723c3ba4eeee90ea4c646912
Alexandr <alex@jurist.vn.ua>
2018-12-17 14:10:37 +0200

    51

--- building.js
+++ building.js
@@ -1,6 +1,6 @@
 function letBuild(roomName) {
   if(Game.getObjectById(Memory.controller).level > 4 ){
-    conslole.log('Conainers and Links = ' + !Memory.linksId);
+    console.log('Continers and Links = ' + !Memory.linksId);
     //if((Memory.containersId.length > 0 &&
     //  Memory.linksId.length <  Memory.containersId.length) || !Memory.linksId){
     //  conslole.log('Conainers and Links = '
