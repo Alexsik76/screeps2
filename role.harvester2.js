15cc43b61efdcda120693bd59d0f66863a3c1dff
Alexandr <alex@jurist.vn.ua>
2018-12-23 21:40:39 +0200

    ee

--- role.harvester2.js
+++ role.harvester2.js
@@ -8,7 +8,7 @@ var roleHarvester2 = {
   if(creep.carry.energy == 0 && !creep.memory.withdraw){
     creep.memory.transport = false;
     creep.memory.withdraw = true;
-    creep.say('⛏ withdraw');
+    creep.say('â withdraw');
   }
   console.log('creep.memory.targeten = ' + creep.memory.targeten);
   console.log('!creep.memory.targeten = ' + !creep.memory.targeten);
