556ccd0e32493957e08e4a16efcec1bd2e0c1a2f
Alexandr <alex@jurist.vn.ua>
2018-12-23 23:03:36 +0200

    wewe

--- let.trade.js
+++ let.trade.js
@@ -1,5 +1,5 @@
 function letTrade(roomName){
-  console.log('terminall = ' + JSON.stringify(Game.rooms[roomName].terminal));
+  console.log('terminall = ' + Game.rooms[roomName].terminal.store[RESOURCE_ENERGY]);
   //let terminal1 = Game.rooms.roomName.terminal.id;
   if(Game.rooms[roomName].terminal.store[RESOURCE_ENERGY] > 2000){
     Game.market.createOrder(ORDER_SELL, RESOURCE_ENERGY, 0.08, 1000, roomName);
