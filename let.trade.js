d8a064c7cb69321e949eb90f564e26438235a020
Alexandr <alex@jurist.vn.ua>
2018-12-23 23:01:56 +0200

    sdsd

--- let.trade.js
+++ let.trade.js
@@ -1,5 +1,7 @@
 function letTrade(roomName){
-  if(Game.rooms.roomName.terminal.store[RESOURCE_ENERGY] > 2000){
+  console.log('terminall = ' + JSON.stringify(Game.rooms[roomName].terminal));
+  let terminal1 = Game.rooms.roomName.terminal.id;
+  if(Game.rooms[roomName].terminal.store[RESOURCE_ENERGY] > 2000){
     Game.market.createOrder(ORDER_SELL, RESOURCE_ENERGY, 0.08, 1000, roomName);
     const amountToBuy = 1000, maxTransferEnergyCost = 500;
     const orders = Game.market.getAllOrders({type: ORDER_SELL, resourceType: RESOURCE_ENERGY});
