1e8caf78be0422348f144021268dfb9ed01713a4
Alexandr <alex@jurist.vn.ua>
2018-12-25 10:43:37 +0200

    t4

--- target.transport4.js
+++ target.transport4.js
@@ -5,17 +5,17 @@ function TargetTransport4(creep){
               filter: (structure) => {
                   return (structure.structureType == STRUCTURE_EXTENSION ||
                           structure.structureType == STRUCTURE_SPAWN ||
-                          structure.structureType == STRUCTURE_TOWER)
-                          && structure.energy < structure.energyCapacity;
+                          structure.structureType == STRUCTURE_TOWER) &&
+                          structure.energy < structure.energyCapacity;
               }
           });
-          if(!target && creep.room.storage.store[RESOURCE_ENERGY] > 400000){
-            return creep.room.terminal.id;
-          } else{
+          if(!target){
+            if(creep.room.storage.store[RESOURCE_ENERGY] > 400000){
+              return creep.room.terminal.id;
+            }
+          } else {
             return target.id;
           }
 
 }
-
-
 module.exports = TargetTransport4;
