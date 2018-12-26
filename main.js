255a9b8e705cc0188f0da5ac69c652c31a3f9ff9
Alexandr <alex@jurist.vn.ua>
2018-12-26 08:29:44 +0200

    -

--- main.js
+++ main.js
@@ -299,7 +299,7 @@ console.log('Not energy for spawn harvester');
     }
 console.log('Energy Avalible = ' + Memory.energyAv);
 console.log('Storage energy = ' + Game.rooms[Memory.roomName].storage.store[RESOURCE_ENERGY]);
-console.table(Game.creeps);
+//console.table(Game.creeps);
 defendRoom(Memory.roomName);
 //letBuild(Memory.roomName);
 
