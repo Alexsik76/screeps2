e142c936623a538e8e59bf9fcdf507dd65bf3c55
Alexandr <alex@jurist.vn.ua>
2018-12-27 13:02:44 +0200

    2

--- role.upgrader2.js
+++ role.upgrader2.js
@@ -15,7 +15,7 @@ var roleUpgrader2 = {
 
     run: function(creep) {
       let posInvasion =  new RoomPosition(3,33, 'E12N47');
-      if(!creep.pos.isNearTo(posInvasion)){
+      if(!creep.room == Memmory.targetInvasion){
       creep.moveTo(posInvasion);
     }else{
             if(creep.memory.upgrading && creep.carry.energy == 0) {
