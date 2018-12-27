f556f31bc9f27c1059ad4fadd2175ebd51d9e0c2
Alexandr <alex@jurist.vn.ua>
2018-12-27 09:08:32 +0200

    trade utrium

--- let.trade.js
+++ let.trade.js
@@ -1,11 +1,11 @@
 function letTrade(roomName){
   console.log('terminall = ' + Game.rooms[roomName].terminal.store[RESOURCE_ENERGY]);
 
-  if(Game.rooms[roomName].terminal.store[RESOURCE_ENERGY] > 2000 &&
+  if(Game.rooms[roomName].terminal.store[RESOURCE_UTRIUM] > 1000 &&
   Game.rooms[roomName].storage.store[RESOURCE_ENERGY] > 400000){
     //Game.market.createOrder(ORDER_SELL, RESOURCE_ENERGY, 0.08, 1000, roomName);
     const amountToBuy = 1000, maxTransferEnergyCost = 800;
-    const orders = Game.market.getAllOrders({type: ORDER_BUY, resourceType: RESOURCE_ENERGY});
+    const orders = Game.market.getAllOrders({type: ORDER_BUY, resourceType: RESOURCE_UTRIUM});
 console.log('terminall orders = ' + orders.length);
     const myOrders = Game.market.getAllOrders({roomName: roomName});
     console.log('terminall My orders = ' + myOrders.length);
