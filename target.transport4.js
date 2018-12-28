13d8d70d041acfd681c039a24af19b4ccb680fef
Alexandr <alex@jurist.vn.ua>
2018-12-28 08:39:53 +0200

    to2

--- target.transport4.js
+++ target.transport4.js
@@ -4,11 +4,18 @@ function TargetTransport4(creep){
       var target = creep.pos.findClosestByRange(FIND_STRUCTURES, {
               filter: (structure) => {
                   return (structure.structureType == STRUCTURE_EXTENSION ||
-                          structure.structureType == STRUCTURE_SPAWN ||
-                          structure.structureType == STRUCTURE_TOWER) &&
+                          structure.structureType == STRUCTURE_SPAWN) &&
                           structure.energy < structure.energyCapacity;
               }
           });
+          if(!target){
+            target = creep.pos.findClosestByRange(FIND_STRUCTURES, {
+                    filter: (structure) => {
+                    return (structure.structureType == STRUCTURE_TOWER) &&
+                    structure.energy < structure.energyCapacity;
+                    }
+                });
+          }
           if(!target){
             if(creep.room.storage.store[RESOURCE_ENERGY] > 400000){
               return creep.room.terminal.id;
