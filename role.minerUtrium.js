4a0cdf4b190d8e2cbe338b2067d998c334f25ded
Alexandr <alex@jurist.vn.ua>
2019-01-02 21:36:58 +0200

    33333

--- role.minerUtrium.js
+++ role.minerUtrium.js
@@ -12,7 +12,7 @@ run: function(creep) {
                 var sources = creep.room.find(FIND_MINERALS);
                 console.log('Utrium =' + sources[0]);
                 if(Game.getObjectById('5c1fd01db4a2ff073249d0fd').cooldown < 1){
-                  creep.harvest(sources[0].resourceType('U'));
+                  creep.harvest(sources[0]);
                   console.log('Utrium2' + creep.harvest(sources));
 
               }
